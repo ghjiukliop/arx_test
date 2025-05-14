7479ff7243b6863bd24134e86bf773c5cf2ee22c
ghjiukliop <152683933+ghjiukliop@users.noreply.github.com>
2025-05-15 01:50:45 +0700

    Update arxnotfinal.lua

--- arxnotfinal.lua
+++ arxnotfinal.lua
@@ -4948,14 +4948,41 @@ local function getUnitListWithTraits()
 end
 
 
- 
-
+-- Danh sách các trait hiện tại
 -- Danh sách các trait hiện tại
 local availableTraits = {
-    "Range", "HP", "Damage", "Sniper", "Brute", "Tank", "Investor", "Jack of All",
-    "Crackshot", "Pure Strength", "Juggernaut", "Banker", "Seraph", "Capitalist",
-    "Duplicator", "Sovereign"
+    "Blitz",
+    "Brute",
+    "Capitalist",
+    "Colossal",
+    "Duplicator",
+    "Endure",
+    "Horizon",
+    "Investor",
+    "Jokester",
+    "Juggernaut",
+    "Millionaire",
+    "Seraph",
+    "Sniper",
+    "Sovereign",
+    "Superior",
+    "Violent"
 }
+
+-- Biến lưu các trait được chọn
+selectedTraits = {}
+
+-- Dropdown cho phép chọn nhiều trait
+TraitRerollSection:AddDropdown("TraitSelectionDropdown", {
+    Title = "Select Traits",
+    Values = availableTraits,
+    Multi = true, -- Cho phép chọn nhiều
+    Default = {}, -- Không chọn gì mặc định
+    Callback = function(selectedValues)
+        selectedTraits = selectedValues
+        print("Các trait đã chọn:", table.concat(selectedTraits, ", "))
+    end
+})
 
 -- Biến lưu các trait được chọn
     selectedTraits = {}
